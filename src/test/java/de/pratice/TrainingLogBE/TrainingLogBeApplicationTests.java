747bf7ab3959ff4722b5ca7c1cd0acecf7b819b8
Christopher <c.hoppe92@t-online.de>
2023-08-03 11:19:51 +0200

    initializing

---
+++ src/test/java/de/pratice/TrainingLogBE/TrainingLogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pratice.TrainingLogBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingLogBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
